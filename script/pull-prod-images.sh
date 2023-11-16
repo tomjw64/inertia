50a3aaf6a05e127cac5ab6bfd7e8c045fbae3a33
tomjw64 <tom.jw64@gmail.com>
2023-11-16 12:30:04 -0600

    Some image push/pull script changes

--- script/pull-prod-images.sh
+++ script/pull-prod-images.sh
@@ -10,11 +10,7 @@ TAG=${TAG:-latest}
 INERTIA_BACKEND_IMAGE="${REPOSITORY}/inertia-backend:${TAG}"
 INERTIA_WEB_IMAGE="${REPOSITORY}/inertia-web:${TAG}"
 
-docker build -f inertia-async-server/dockerfile -t ${INERTIA_BACKEND_IMAGE} --target prod .
-docker build -f inertia-web/dockerfile -t ${INERTIA_WEB_IMAGE} --target prod .
-
-aws sso login
 aws ecr get-login-password | docker login --username AWS --password-stdin ${REPOSITORY}
 
-docker push ${INERTIA_BACKEND_IMAGE}
-docker push ${INERTIA_WEB_IMAGE}
+docker pull ${INERTIA_BACKEND_IMAGE}
+docker pull ${INERTIA_WEB_IMAGE}
