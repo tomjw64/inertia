b1666dff458b812a80cddb304f664054cd69fd5a
thomas <tom.jw64@gmail.com>
2024-05-18 18:08:27 -0500

    Add ValueOf type util

--- inertia-web/src/utils/types.ts
+++ inertia-web/src/utils/types.ts
@@ -1,5 +1,7 @@
 import { Dispatch, StateUpdater } from 'preact/hooks';
 
+export type ValueOf<T> = T[keyof T];
+
 export type StateSetter<T> = Dispatch<StateUpdater<T>>;
 
 export type Nullable<T> = T | null | undefined;
