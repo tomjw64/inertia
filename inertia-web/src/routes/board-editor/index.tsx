beff989311b913c8f3009e79ef3273e5dc58df69
thomas <tom.jw64@gmail.com>
2024-05-18 18:12:28 -0500

    Use alt Board component for Board Editor

--- inertia-web/src/routes/board-editor/index.tsx
+++ inertia-web/src/routes/board-editor/index.tsx
@@ -1,16 +1,23 @@
-import { Position } from 'inertia-core';
+import { ActorSquares, Position, WalledBoard } from 'inertia-core';
 import { decode_position, encode_position } from 'inertia-wasm';
 import debounce from 'lodash/debounce';
 import { useEffect, useMemo, useState } from 'preact/hooks';
-import { Board } from '../../components/board';
 import { Divider } from '../../components/divider';
+import {
+  BoardSelection,
+  SimpleBoard,
+  SquareMouseEvent,
+  SquareRegionType,
+  isActorSelection,
+} from '../../components/simple-board';
 import { ErrorPage } from '../../components/error-page';
 import { FlexCenter } from '../../components/flex-center';
 import { PanelTitle } from '../../components/panel-title';
 import { AppControls } from '../../components/room-controls';
 import { Starfield } from '../../components/starfield';
+import { ThemedButton } from '../../components/themed-form';
 import { ThemedPanel } from '../../components/themed-panel';
-import { defaultPositionBytes } from '../../utils/board';
+import { defaultPositionBytes, emptyBoard } from '../../utils/board';
 
 const debouncedSetUrlParams = debounce((params: URLSearchParams) => {
   const currentState = window.history.state;
@@ -18,6 +25,27 @@ const debouncedSetUrlParams = debounce((params: URLSearchParams) => {
   const newUrl = currentUrl.split('?')[0] + '?' + params.toString();
   window.history.replaceState(currentState, '', newUrl);
 }, 200);
+
+// TODO: use wasm and reuse logic from walled_board.rs (requires internet)?
+const toggleWall = (
+  walls: WalledBoard,
+  row: number,
+  column: number,
+  region: SquareRegionType,
+) => {
+  if (region === SquareRegionType.CENTER) {
+    return;
+  }
+  if (region === SquareRegionType.LEFT && column !== 0) {
+    walls.vertical[row][column - 1] = !walls.vertical[row][column - 1];
+  } else if (region === SquareRegionType.RIGHT && column !== 15) {
+    walls.vertical[row][column] = !walls.vertical[row][column];
+  } else if (region === SquareRegionType.TOP && row !== 0) {
+    walls.horizontal[column][row - 1] = !walls.horizontal[column][row - 1];
+  } else if (region === SquareRegionType.BOTTOM && row !== 15) {
+    walls.horizontal[column][row] = !walls.horizontal[column][row];
+  }
+};
 
 export const BoardEditor = () => {
   const originalOrDefaultPosition = useMemo(() => {
@@ -27,7 +55,24 @@ export const BoardEditor = () => {
     return decode_position(originalPositionBytes ?? defaultPositionBytes);
   }, []);
 
+  const [selection, setSelection] = useState<BoardSelection>(
+    BoardSelection.NONE,
+  );
   const [position, setPosition] = useState<Position>(originalOrDefaultPosition);
+  const [mouseOverIndicatorWall, setMouseOverIndicatorWall] =
+    useState<WalledBoard>(emptyBoard());
+
+  useEffect(() => {
+    const listener = () => {
+      // Click events don't propogate from the board
+      setSelection(BoardSelection.NONE);
+    };
+    document.addEventListener('click', listener);
+    return () => {
+      document.removeEventListener('click', listener);
+    };
+  });
+
   useEffect(() => {
     const urlParams = new URLSearchParams();
     if (position) {
@@ -35,6 +80,7 @@ export const BoardEditor = () => {
     }
     debouncedSetUrlParams(urlParams);
   }, [position]);
+
   if (!position) {
     return (
       <>
@@ -43,6 +89,84 @@ export const BoardEditor = () => {
       </>
     );
   }
+
+  const openBoardExplorer = () => {
+    const boardExplorerParams = new URLSearchParams();
+    boardExplorerParams.append('position', encode_position(position));
+    const boardExplorerUrl = `/explore?${boardExplorerParams.toString()}`;
+    window.open(boardExplorerUrl, '_blank');
+  };
+
+  const handleClickRegion = (event: SquareMouseEvent) => {
+    const { squareIndex, region, row, column } = event;
+
+    if (
+      selection === BoardSelection.NONE &&
+      region !== SquareRegionType.CENTER
+    ) {
+      const currentWalls = structuredClone(position.walled_board);
+      toggleWall(currentWalls, row, column, region);
+      setPosition({
+        ...position,
+        walled_board: currentWalls,
+      });
+      return;
+    }
+
+    const selectingActorIndex = position.actor_squares.indexOf(squareIndex);
+    if (selectingActorIndex !== -1) {
+      setSelection(
+        selection === selectingActorIndex
+          ? BoardSelection.NONE
+          : selectingActorIndex,
+      );
+      return;
+    }
+
+    const isSelectingGoal = position.goal === squareIndex;
+    if (isSelectingGoal) {
+      setSelection(
+        selection === BoardSelection.GOAL
+          ? BoardSelection.NONE
+          : BoardSelection.GOAL,
+      );
+      return;
+    }
+
+    if (isActorSelection(selection)) {
+      const newActorSquares = [...position.actor_squares] as ActorSquares;
+      newActorSquares[selection] = squareIndex;
+      setPosition({
+        ...position,
+        actor_squares: newActorSquares,
+      });
+      setSelection(BoardSelection.NONE);
+      return;
+    }
+
+    if (selection === BoardSelection.GOAL) {
+      setPosition({
+        ...position,
+        goal: squareIndex,
+      });
+      setSelection(BoardSelection.NONE);
+      return;
+    }
+  };
+
+  const handleMouseEnterRegion = (event: SquareMouseEvent) => {
+    if (selection !== BoardSelection.NONE) {
+      return;
+    }
+    const { row, column, region } = event;
+    const indicatorWall = emptyBoard();
+    toggleWall(indicatorWall, row, column, region);
+    setMouseOverIndicatorWall(indicatorWall);
+  };
+
+  const handleMouseLeaveBoard = () => {
+    setMouseOverIndicatorWall(emptyBoard());
+  };
 
   return (
     <>
@@ -53,12 +177,20 @@ export const BoardEditor = () => {
           <FlexCenter column>
             <PanelTitle>Board Editor</PanelTitle>
             <Divider />
+            <ThemedButton onClick={openBoardExplorer}>
+              View in Board Explorer
+            </ThemedButton>
           </FlexCenter>
         </ThemedPanel>
-        <Board
+        <SimpleBoard
           walledBoard={position.walled_board}
           goal={position.goal}
           actorSquares={position.actor_squares}
+          selection={selection}
+          indicatorWalls={mouseOverIndicatorWall}
+          onClickRegion={handleClickRegion}
+          onMouseEnterRegion={handleMouseEnterRegion}
+          onMouseLeaveBoard={handleMouseLeaveBoard}
         />
       </FlexCenter>
     </>
