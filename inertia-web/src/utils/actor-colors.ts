285b2efbd8a614025c5dde35dc0be03e175a35a7
thomas <tom.jw64@gmail.com>
2024-05-18 18:09:04 -0500

    Add actor color and index utils

--- inertia-web/src/utils/actor-colors.ts
+++ inertia-web/src/utils/actor-colors.ts
@@ -1,4 +1,11 @@
-const ACTOR_COLORS = ['red', 'blue', 'green', 'yellow'];
+const ACTOR_COLORS = ['red', 'blue', 'green', 'yellow'] as const;
+
+export type ActorColor = (typeof ACTOR_COLORS)[number];
+
 export const getActorColor = (actorIndex: number) => {
   return ACTOR_COLORS[actorIndex];
+};
+
+export const getActorIndex = (color: ActorColor) => {
+  return ACTOR_COLORS.indexOf(color);
 };
