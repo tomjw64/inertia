7b75245ee3cb25294a36831e6db1c3fbd26ef7ff
tomjw64 <tom.jw64@gmail.com>
2024-10-19 23:14:38 -0500

    Move threejs-starfield to webworker

--- inertia-web/src/components/starfield/threejs-starfield.tsx
+++ inertia-web/src/components/starfield/threejs-starfield.tsx
@@ -1,56 +1,12 @@
-import { useRef, useEffect, useMemo, useCallback } from 'preact/hooks';
+import { useRef, useEffect } from 'preact/hooks';
 import style from './style.module.scss';
 import { debounce } from 'lodash';
-import {
-  Color,
-  CylinderGeometry,
-  FogExp2,
-  Frustum,
-  Matrix4,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  Scene,
-  Vector3,
-  WebGLRenderer,
-  WebGLRenderTarget,
-} from 'three';
-import {
-  EffectComposer,
-  FXAAShader,
-  GammaCorrectionShader,
-  RenderPass,
-  ShaderPass,
-} from 'three/addons';
-import { useLazyEffectRef } from '../../utils/hooks/use-lazy-ref';
-
-const getColorString = (r: number, g: number, b: number) =>
-  `#${((r << 16) | (g << 8) | b).toString(16)}`;
-
-const getCanvasHeight = () =>
-  Math.floor(window.innerHeight * window.devicePixelRatio);
 
 const getCanvasWidth = () =>
   Math.floor(window.innerWidth * window.devicePixelRatio);
 
-const getCanvasAspectRatio = () => getCanvasWidth() / getCanvasHeight();
-
-const Z_LIMIT = 1000;
-
-const STAR_R = 0xff;
-const STAR_G = 0xff;
-const STAR_B = 0xff;
-
-const STAR_COLOR = getColorString(STAR_R, STAR_B, STAR_G);
-
-const RADIAL_GEOMETRY_SEGMENTS = 16;
-const STAR_GEOMETRY = new CylinderGeometry(
-  1,
-  1,
-  1,
-  RADIAL_GEOMETRY_SEGMENTS,
-).rotateX(Math.PI / 2);
-const STAR_MATERIAL = new MeshBasicMaterial({ color: STAR_COLOR });
+const getCanvasHeight = () =>
+  Math.floor(window.innerHeight * window.devicePixelRatio);
 
 export const Starfield = ({
   numStars,
@@ -59,189 +15,49 @@ export const Starfield = ({
   numStars: number;
   speed: number;
 }) => {
-  const canvas = useRef<HTMLCanvasElement>(null);
-
-  const stars = useRef<
-    {
-      mesh: Mesh;
-      relativeCoords: { x: number; y: number };
-    }[]
-  >([]);
-
-  const animationFrame = useRef<number>(0);
-
-  const camera = useLazyEffectRef<PerspectiveCamera>(
-    () => new PerspectiveCamera(50, getCanvasAspectRatio(), 50, Z_LIMIT),
-  );
-  const getFrustumFromCamera = useCallback(() => {
-    const frustum = new Frustum();
-    frustum.setFromProjectionMatrix(
-      new Matrix4().multiplyMatrices(
-        camera.current!.projectionMatrix,
-        camera.current!.matrixWorldInverse,
-      ),
-    );
-    return frustum;
-  }, [camera]);
-  const frustum = useLazyEffectRef<Frustum>(getFrustumFromCamera);
-  const getCornersFromCamera = useCallback(() => {
-    return {
-      topLeft: new Vector3().set(-1, 1, 1).unproject(camera.current!),
-      bottomRight: new Vector3().set(1, -1, 1).unproject(camera.current!),
-    };
-  }, [camera]);
-  const corners = useLazyEffectRef<{
-    topLeft: Vector3;
-    bottomRight: Vector3;
-  }>(getCornersFromCamera);
-  const scene = useLazyEffectRef<Scene | null>(() => {
-    const scene = new Scene();
-    scene.background = new Color(0x373b55);
-    scene.fog = new FogExp2(0x373b55, 0.0025);
-    return scene;
-  });
-  const renderer = useLazyEffectRef<WebGLRenderer | null>(() => {
-    const renderer = new WebGLRenderer({
-      canvas: canvas.current!,
-    });
-    return renderer;
-  });
-  const composer = useLazyEffectRef<EffectComposer | null>(() => {
-    const composer = new EffectComposer(
-      renderer.current!,
-      new WebGLRenderTarget(getCanvasWidth(), getCanvasHeight()),
-    );
-
-    const renderPass = new RenderPass(scene.current!, camera.current!);
-
-    const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
-    const fxaaPass = new ShaderPass(FXAAShader);
-    fxaaPass.material.uniforms['resolution']!.value.x = 1 / getCanvasWidth();
-    fxaaPass.material.uniforms['resolution']!.value.y = 1 / getCanvasHeight();
-
-    composer.addPass(renderPass);
-    composer.addPass(gammaCorrectionPass);
-    composer.addPass(fxaaPass);
-    return composer;
-  });
-
-  const recalculateMeshXY = useCallback(
-    (mesh: Mesh, relativeCoords: { x: number; y: number }) => {
-      const { topLeft, bottomRight } = corners.current!;
-      mesh.position.x =
-        relativeCoords.x * (bottomRight.x - topLeft.x) + topLeft.x;
-      mesh.position.y =
-        relativeCoords.y * (bottomRight.y - topLeft.y) + topLeft.y;
-    },
-    [corners],
-  );
-
-  const handleResize = useMemo(
-    () =>
-      debounce(() => {
-        camera.current!.aspect = getCanvasAspectRatio();
-        camera.current!.updateProjectionMatrix();
-        renderer.current!.setSize(getCanvasWidth(), getCanvasHeight());
-        composer.current!.setSize(getCanvasWidth(), getCanvasHeight());
-        frustum.current = getFrustumFromCamera();
-        corners.current = getCornersFromCamera();
-
-        for (const { mesh, relativeCoords } of stars.current) {
-          recalculateMeshXY(mesh, relativeCoords);
-        }
-      }, 200),
-    [
-      camera,
-      composer,
-      corners,
-      frustum,
-      getCornersFromCamera,
-      getFrustumFromCamera,
-      recalculateMeshXY,
-      renderer,
-    ],
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const workerRef = useRef(
+    new Worker(new URL('./threejs-starfield.worker.ts', import.meta.url)),
   );
 
   useEffect(() => {
-    handleResize();
+    const canvas = canvasRef.current!.transferControlToOffscreen();
+    const worker = workerRef.current;
+    worker.postMessage(
+      {
+        canvas,
+        canvasWidth: getCanvasWidth(),
+        canvasHeight: getCanvasHeight(),
+      },
+      [canvas],
+    );
+
+    return () => {
+      worker.terminate();
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      workerRef.current.postMessage({
+        canvasWidth: getCanvasWidth(),
+        canvasHeight: getCanvasHeight(),
+      });
+    }, 200);
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [handleResize]);
+  }, []);
 
   useEffect(() => {
-    for (const { mesh } of stars.current) {
-      mesh.scale.z = speed;
-    }
-  }, [speed]);
-
-  useEffect(() => {
-    if (stars.current.length > numStars) {
-      for (let i = numStars; i < stars.current.length; i++) {
-        scene.current!.remove(stars.current[i]!.mesh);
-      }
-      stars.current.length = numStars;
-    } else if (stars.current.length < numStars) {
-      for (let i = stars.current.length; i < numStars; i++) {
-        const mesh = new Mesh(STAR_GEOMETRY, STAR_MATERIAL);
-        const relativeCoords = {
-          x: Math.random(),
-          y: Math.random(),
-        };
-        recalculateMeshXY(mesh, relativeCoords);
-        mesh.scale.z = speed;
-        mesh.position.z = -1 * Z_LIMIT * Math.random();
-        stars.current.push({
-          mesh,
-          relativeCoords,
-        });
-        scene.current!.add(mesh);
-      }
-    }
-  }, [numStars, recalculateMeshXY, scene, speed]);
-
-  useEffect(() => {
-    const animateMotion = (_timestamp: number) => {
-      for (const { mesh, relativeCoords } of stars.current) {
-        mesh.position.z += speed;
-
-        if (
-          mesh.position.z > 0 ||
-          !frustum.current!.containsPoint(mesh.position)
-        ) {
-          relativeCoords.x = Math.random();
-          relativeCoords.y = Math.random();
-          recalculateMeshXY(mesh, relativeCoords);
-          mesh.position.z = -1 * Z_LIMIT;
-        }
-      }
-      composer.current!.render();
-
-      animationFrame.current = window.requestAnimationFrame(animateMotion);
-    };
-
-    animateMotion(0);
-
-    return () => {
-      if (animationFrame.current != 0) {
-        window.cancelAnimationFrame(animationFrame.current);
-      }
-    };
-  }, [
-    camera,
-    composer,
-    frustum,
-    numStars,
-    recalculateMeshXY,
-    renderer,
-    scene,
-    speed,
-  ]);
+    const worker = workerRef.current;
+    worker.postMessage({ numStars, speed });
+  }, [numStars, speed]);
 
   return (
     <div className={style.background}>
-      <canvas className={style.canvas} ref={canvas} />;
+      <canvas className={style.canvas} ref={canvasRef} />
     </div>
   );
 };
---
+++ inertia-web/src/components/starfield/threejs-starfield.worker.ts
@@ -0,0 +1,212 @@
+import {
+  Color,
+  CylinderGeometry,
+  FogExp2,
+  Frustum,
+  Matrix4,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  Vector3,
+  WebGLRenderer,
+} from 'three';
+
+const getColorString = (r: number, g: number, b: number) =>
+  `#${((r << 16) | (g << 8) | b).toString(16)}`;
+
+const Z_LIMIT = 1000;
+
+const STAR_R = 0xff;
+const STAR_G = 0xff;
+const STAR_B = 0xff;
+
+const STAR_COLOR = getColorString(STAR_R, STAR_B, STAR_G);
+
+const RADIAL_GEOMETRY_SEGMENTS = 16;
+const STAR_GEOMETRY = new CylinderGeometry(
+  1,
+  1,
+  1,
+  RADIAL_GEOMETRY_SEGMENTS,
+).rotateX(Math.PI / 2);
+const STAR_MATERIAL = new MeshBasicMaterial({ color: STAR_COLOR });
+
+class StarfieldAnimation {
+  private stars: {
+    mesh: Mesh;
+    relativeCoords: { x: number; y: number };
+  }[];
+  private speed: number;
+
+  private camera: PerspectiveCamera;
+  private frustum: Frustum;
+  private corners: {
+    topLeft: Vector3;
+    bottomRight: Vector3;
+  };
+  private scene: Scene;
+  private renderer: WebGLRenderer;
+
+  constructor(
+    private canvas: HTMLCanvasElement,
+    private canvasWidth: number,
+    private canvasHeight: number,
+  ) {
+    this.stars = [];
+    this.speed = 0;
+
+    this.camera = this.createCamera();
+    this.frustum = this.getFrustumFromCurrentCamera();
+    this.corners = this.getCornersFromCurrentCamera();
+    this.scene = this.createScene();
+    this.renderer = this.createRenderer();
+  }
+
+  private getCanvasAspectRatio() {
+    return this.canvasWidth / this.canvasHeight;
+  }
+
+  private createCamera() {
+    return new PerspectiveCamera(50, this.getCanvasAspectRatio(), 50, Z_LIMIT);
+  }
+
+  private createScene() {
+    const scene = new Scene();
+    scene.background = new Color(0x373b55);
+    scene.fog = new FogExp2(0x373b55, 0.0025);
+    return scene;
+  }
+
+  private createRenderer() {
+    return new WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+  }
+
+  private render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  private getFrustumFromCurrentCamera() {
+    const frustum = new Frustum();
+    frustum.setFromProjectionMatrix(
+      new Matrix4().multiplyMatrices(
+        this.camera.projectionMatrix,
+        this.camera.matrixWorldInverse,
+      ),
+    );
+    return frustum;
+  }
+
+  private getCornersFromCurrentCamera() {
+    return {
+      topLeft: new Vector3().set(-1, 1, 1).unproject(this.camera),
+      bottomRight: new Vector3().set(1, -1, 1).unproject(this.camera),
+    };
+  }
+
+  private recalculateMeshXY(
+    mesh: Mesh,
+    relativeCoords: { x: number; y: number },
+  ) {
+    const { topLeft, bottomRight } = this.corners;
+    mesh.position.x =
+      relativeCoords.x * (bottomRight.x - topLeft.x) + topLeft.x;
+    mesh.position.y =
+      relativeCoords.y * (bottomRight.y - topLeft.y) + topLeft.y;
+  }
+
+  handleResize(width: number, height: number) {
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height, false);
+    this.frustum = this.getFrustumFromCurrentCamera();
+    this.corners = this.getCornersFromCurrentCamera();
+
+    for (const { mesh, relativeCoords } of this.stars) {
+      this.recalculateMeshXY(mesh, relativeCoords);
+    }
+  }
+
+  setSpeed(speed: number) {
+    this.speed = speed;
+    for (const { mesh } of this.stars) {
+      mesh.scale.z = speed;
+    }
+  }
+
+  setNumStars(numStars: number) {
+    if (this.stars.length > numStars) {
+      for (let i = numStars; i < this.stars.length; i++) {
+        this.scene.remove(this.stars[i]!.mesh);
+      }
+      this.stars.length = numStars;
+    } else if (this.stars.length < numStars) {
+      for (let i = this.stars.length; i < numStars; i++) {
+        const mesh = new Mesh(STAR_GEOMETRY, STAR_MATERIAL);
+        const relativeCoords = {
+          x: Math.random(),
+          y: Math.random(),
+        };
+        this.recalculateMeshXY(mesh, relativeCoords);
+        mesh.scale.z = this.speed;
+        mesh.position.z = -1 * Z_LIMIT * Math.random();
+        this.stars.push({
+          mesh,
+          relativeCoords,
+        });
+        this.scene.add(mesh);
+      }
+    }
+  }
+
+  animate() {
+    const animateMotion = (_timestamp: number) => {
+      for (const { mesh, relativeCoords } of this.stars) {
+        mesh.position.z += this.speed;
+
+        if (mesh.position.z > 0 || !this.frustum.containsPoint(mesh.position)) {
+          relativeCoords.x = Math.random();
+          relativeCoords.y = Math.random();
+          this.recalculateMeshXY(mesh, relativeCoords);
+          mesh.position.z = -1 * Z_LIMIT;
+        }
+      }
+      this.render();
+
+      requestAnimationFrame(animateMotion);
+    };
+
+    animateMotion(0);
+  }
+}
+
+type MessageData = {
+  canvas?: HTMLCanvasElement;
+  canvasWidth: number;
+  canvasHeight: number;
+  numStars?: number;
+  speed?: number;
+};
+
+let starfield: StarfieldAnimation | undefined;
+addEventListener('message', (msg) => {
+  const { canvas, canvasWidth, canvasHeight, numStars, speed } =
+    msg.data as MessageData;
+
+  if (canvas && canvasWidth != null && canvasHeight != null) {
+    starfield = new StarfieldAnimation(canvas, canvasWidth, canvasHeight);
+    starfield.animate();
+  }
+  if (canvasWidth != null && canvasHeight != null) {
+    starfield?.handleResize(canvasWidth, canvasHeight);
+  }
+  if (speed != null) {
+    starfield?.setSpeed(speed);
+  }
+  if (numStars != null) {
+    starfield?.setNumStars(numStars);
+  }
+});
